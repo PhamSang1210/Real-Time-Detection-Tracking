e543951e5473dd342994658b0e808eb2ca8bf9d9
PhamSang1210 <phamsang1210z9@gmail.com>
2024-10-25 16:21:41 +0700

    .

--- app.py
+++ app.py
@@ -181,37 +181,3 @@ if __name__ == "__main__":
         frame = predict(model, deepsort, frame)
         frame= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         stframe.image(frame)
-
-
-
-  
-# # define a video capture object
-# vid = cv2.VideoCapture(0)
-  
-# while(True):
-      
-#     # Capture the video frame
-#     # by frame
-#     ret, frame = vid.read()
-#     frame = predict(model, deepsort, frame)
-#     # Display the resulting frame
-#     cv2.imshow('frame', frame)
-      
-#     # the 'q' button is set as the
-#     # quitting button you may use any
-#     # desired button of your choice
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-  
-# # After the loop release the cap object
-# vid.release()
-# # Destroy all the windows
-# cv2.destroyAllWindows()
-
-
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/toanphanminh/realtime-tracking.git
-# git push -u origin main
